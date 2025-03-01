85b15984e5c1988bf680f3e00b50231e3172e9a7
unknown <noka204>
2025-03-01 11:12:37 +0700

    [update] ex3

--- BaiTap_1/ex03/ex03_05.py
+++ BaiTap_1/ex03/ex03_05.py
@@ -1,9 +1,9 @@
 def dem_so_lan_xuat_hien(lst):
     count_dict = {}
-    for item in count_dict:
+    for item in lst:
         if item in count_dict:
             count_dict[item] += 1
-        else :
+        else:
             count_dict[item] = 1
     return count_dict
 input_string = input("Nhập danh sách các từ, cách nhau bằng dấu phẩy : ")
